08e698b3fd9195f4a04a1ac41898da33ee552666
GabrielCam123 <cameotapia@gmail.com>
2025-06-09 09:24:42 -0400

    Sharktank2

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 console.log("BACKEND URL:", import.meta.env.VITE_IP_LOCAL_BACKEND);
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_IP_LOCAL_BACKEND,
+  baseURL: "https://tudespensa-backend.onrender.com/api",
   //baseURL: "https://backend-production-e141.up.railway.app/api",
   withCredentials: true,
 });
